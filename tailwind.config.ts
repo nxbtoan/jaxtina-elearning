d9c71c3e702492d63e258d83568ad1f7ddc7e3ab
Bảo Toàn Xuân Nguyễn <nxbtoan175@gmail.com>
2025-10-27 22:02:03 +0700

    chore: Initial project setup and configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF", // blue-800
+        secondary: "#2563EB", // blue-600
+        accent: "#FACC15", // yellow-400
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+};
+export default config;
